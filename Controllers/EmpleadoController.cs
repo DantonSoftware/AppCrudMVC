d46b9a67e6fd1d84f4c614a13513fe195511efa8
Danton <danton.cortes@gmail.com>
2024-12-27 15:57:56 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/EmpleadoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppCrud.Data;
+using AppCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppCrud.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        private readonly AppDBContext _appDbContext;
+
+        public EmpleadoController(AppDBContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<Empleado> lista = await _appDbContext.Empleados.ToListAsync();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Nuevo()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Nuevo(Empleado empleado)
+        {
+            await _appDbContext.Empleados.AddAsync(empleado);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Lista));
+        }
+    }
+}
